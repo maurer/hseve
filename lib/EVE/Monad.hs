3d9750144a184d4e2a03943310cef0b8f67688ac
Matthew Maurer <maurer@matthewmaurer.org>
2012-07-19 14:07:19 -0400

    Enabled cache engine, but not hooked in yet

--- lib/EVE/Monad.hs
+++ lib/EVE/Monad.hs
@@ -2,14 +2,18 @@ module EVE.Monad where
 
 import Control.Monad.Error
 import Control.Monad.Reader
+import Control.Monad.State
+import Text.XML
+import EVE.TimedCache
 import Data.Typeable
+import Data.Time
 
 data EVECred = EC { userID :: Int
                   , apiKey :: String
-      }
+      } deriving (Ord, Eq)
 
 data EVEParam = EInt Int
-              | EStr String
+              | EStr String deriving (Ord, Eq)
 
 data CorpID = CoID {coidInner :: Int} deriving Show
 data CharID = ChID {chidInner :: Int} deriving Show
@@ -23,11 +27,31 @@ data EVEError = StructError
 
 instance Error EVEError where
   strMsg = OtherError
+--TODO For memory performance, I should try to move the cache out of
+--the xml layer. This is easier for now though.
+type Query = (EVECred, (String, String, [(String, EVEParam)]))
+type Response = Element
+type Cache = TimedCache Query Response
 
-type EVE = ErrorT EVEError (ReaderT EVECred IO)
+type EVE = ErrorT EVEError (ReaderT EVECred (StateT Cache IO))
 
+-- | Runs an action in the EVE monad. Note that this currently
+--   uses a new cache manager per-invocation, so you should not
+--   call this frequently, instead preferring to embed all your
+--   calls into a single monadic action.
 runEVE :: EVECred -> EVE a -> IO (Either EVEError a)
-runEVE c m = runReaderT (runErrorT m) c
+runEVE c m = evalStateT (runReaderT (runErrorT m) c) empty
 
 getCreds :: EVE EVECred
 getCreds = ask
+
+eveCacheReg :: Query -> Response -> NominalDiffTime -> EVE ()
+eveCacheReg q r dt = do
+  t0 <- liftIO $ getCurrentTime
+  modify $ cacheReg q r $ addUTCTime dt t0
+
+eveCacheCheck :: Query -> EVE (Maybe Response)
+eveCacheCheck q = do
+  s <- get
+  t <- liftIO $ getCurrentTime
+  state $ cacheCheck q t
---
+++ lib/EVE/TimedCache.hs
@@ -0,0 +1,20 @@
+module EVE.TimedCache where
+
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Time
+
+type TimedCache query response = Map query (response, UTCTime)
+
+empty = Map.empty
+
+cacheReg :: (Ord q) => q -> r -> UTCTime -> TimedCache q r -> TimedCache q r
+cacheReg k v t c = Map.insert k (v, t) c
+
+cacheCheck :: (Ord q) => q -> UTCTime -> TimedCache q r -> (Maybe r, TimedCache q r)
+cacheCheck k t m = let
+  m' = Map.filter (\(_,t') -> t < t') m
+  in (fmap fst $ Map.lookup k m', m')
+
+cacheMerge :: (Ord q) => [TimedCache q r] -> TimedCache q r
+cacheMerge = Map.unions
