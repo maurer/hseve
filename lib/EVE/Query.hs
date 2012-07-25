76dbf4a30f60e1abb293a0d4c29739112bc4392e
Matthew Maurer <maurer@matthewmaurer.org>
2012-07-25 03:28:47 -0400

    Added documentation to EVE.Query

--- lib/EVE/Query.hs
+++ lib/EVE/Query.hs
@@ -1,3 +1,5 @@
+-- | This module contains the implementations of all the different
+--   individual queries. This may be broken down by category later.
 module EVE.Query where
 
 import EVE.Monad
@@ -5,29 +7,37 @@ import EVE.Query.XML
 import EVE.Query.Types
 import Data.Time
 
+-- | Represents the handles to a character's information
 data CharIdent = CI { charName :: String -- ^ Character name
                     , charID   :: CharID -- ^ Character ID
                     , corpName :: String -- ^ Corp Name
                     , corpID   :: CorpID -- ^ Corp ID
                     }
 
-getCharacters :: EVECred  -- ^ Credentials
+-- | Takes in credentials, provides a list of character info they
+--   are willing to accept these for.
+getCharacters :: EVECred         -- ^ Credentials
               -> EVE [CharIdent] -- ^ Character ID records
-getCharacters ec = mapRowset charExtract $ eveQuery ec "account" "characters" []
-   where charExtract row = do
-           name <- row !* "name"
-           chid <- fmap ChID $ readE =<< row !* "characterID"
-           corp <- row !* "corporationName"
-           coid <- fmap CoID $ readE =<< row !* "corporationID"
-           return $ CI name chid corp coid
+getCharacters ec =
+  mapRowset charExtract $ eveQuery ec "account" "characters" []
+  where charExtract row = do
+          name <- row !* "name"
+          chid <- fmap ChID $ readE =<< row !* "characterID"
+          corp <- row !* "corporationName"
+          coid <- fmap CoID $ readE =<< row !* "corporationID"
+          return $ CI name chid corp coid
 
-data AccountStatus = AcStat { paidUntil    :: UTCTime
-                            , createDate   :: UTCTime
-                            , logonCount   :: Int
-                            , logonMinutes :: Int
-                            } deriving Show
+-- | Contains information about billing and playtime.
+data AccountStatus =
+  AcStat { paidUntil    :: UTCTime -- ^ Expiry time
+         , createDate   :: UTCTime -- ^ Account creation time
+         , logonCount   :: Int -- ^ Number of times logged in
+         , logonMinutes :: Int -- ^ Amount of time in-game
+         } deriving Show
 
-getAccStatus :: EVECred -> EVE AccountStatus
+-- | For user keys, provides information about billing and playtime
+getAccStatus :: EVECred -- ^ User credentials
+             -> EVE AccountStatus
 getAccStatus ec = do
   r <- eveQuery ec "account" "AccountStatus" []
   pu <- readContents r "paidUntil"
